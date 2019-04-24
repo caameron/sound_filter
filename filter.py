5d34d3b80010ae784de20c4f6499b61f1dcff1c1
caameron <30531788+caameron@users.noreply.github.com>
2019-04-24 09:00:49 -0700

    Added in more comments to explain hw

--- filter.py
+++ filter.py
@@ -12,4 +12,9 @@ def filter(samples, length, frequency, rate):
 length is equal to N in the article ? I think
 returns two things? freqs and results
 
+Should use the optimized filter that was in the embedded article it is easier and faster.
+
+when the filter is run you get the maginitude back. That magnitude shoud spike at the target frequency
+all other values should form a bell shaped curve. 
+FIgure out the constants. Target frequency, block size, length, and so on. 
 '''
