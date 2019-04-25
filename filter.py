8af9bd9698637216d80c2e0dc02ad1bfb570a5e4
CaameronNakasone <caameron@pdx.edu>
2019-04-25 09:53:46 -0700

    close to finishing filter

--- filter.py
+++ filter.py
@@ -2,13 +2,17 @@ import numpy
 
 RATE = 48000
 
-'''https://www.embedded.com/design/configurable-systems/4024443/The-Goertzel-Algorithm'''
+'''Implementation is of optimized version from
+   https://www.embedded.com/design/configurable-systems/4024443/The-Goertzel-Algorithm
+'''
 
 class constants:
     '''class to hold data on  norm and coefficients'''
     coef = 0
     cosine = 0
     sine = 0
+    k = 0
+    w0 = 0
     def compute(self, freq, n):
         k = ((n * freq)/ RATE)
         w0 = ((2*numpy.pi*k) / n)
@@ -20,11 +24,20 @@ class constants:
 def filter(samples, length, target):
 
     '''w0 is the target frequnecy of filter in radians'''
-    c1 = constants()
-    x = c1.compute(target, length)
+    constant = constants()
+    x = constant.compute(target, length)
 
     '''Compute each sample'''
+    first = 0
+    second = 0
+    third = 0
+    for sample in samples:
+        first = constant.coef * second - third + sample
+        third = second
+        second = first
 
+    '''compute optimized magnitude'''
+    mag = nupmy.power(second,2) + numpy.power(third,2) - second * third * constant.coef
 
 '''
 160 samples per bit
@@ -36,4 +49,5 @@ returns two things? freqs and results
 
 '''
 
-filter(1, 205, 941)
+print(numpy.power(5,2))
+print(5*5)
